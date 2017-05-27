e890aa42c6fe2db6cbe7c4e9c60f7ca6fc42c9c5
Travis CI <travis@travis-ci.com>
2017-05-27 12:41:41 +0000

    Travis deploy 224

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+importScripts('./build/workbox-sw.prod.v1.0.0.js');
+
+const workboxSW = new WorkboxSW({ clientsClaim: true });
+
+/**
+ * This array will be populated by workboxBuild.injectManifest() when the
+ * production service worker is generated.
+ */
+workboxSW.precache([
+  {
+    "url": "build/main.js",
+    "revision": "e932acd32396bf22a45c24e2f14cf5ad"
+  },
+  {
+    "url": "build/polyfills.js",
+    "revision": "6da628b2e650e6734c5a204397fc942a"
+  },
+  {
+    "url": "build/workbox-sw.prod.v1.0.0.js",
+    "revision": "9029a00430d1c6ccf363f3ad77c45d42"
+  },
+  {
+    "url": "build/main.css",
+    "revision": "20568e5600f7a7f3c021e2c0248e34fb"
+  },
+  {
+    "url": "index.html",
+    "revision": "6b8eab0bc421bd5fe670e850c101311c"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "445711282b172515ad9a87e942266e8e"
+  }
+]);
+
+workboxSW.router.registerRoute(
+  '/(.*)',
+  workboxSW.strategies.cacheFirst()
+);
+
+workboxSW.router.registerRoute(
+  'https://csrdelft.nl/API/(.*)',
+  workboxSW.strategies.networkFirst()
+);
+
+workboxSW.router.registerRoute(
+  'https://csrdelft.nl/plaetjes/(.*)',
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'plaetjes',
+    cacheExpiration: {
+      maxAgeSeconds: 28 * 24 * 60 * 60,
+    },
+    cacheableResponse: {
+      statuses: [0, 200]
+    }
+  })
+);
